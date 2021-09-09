39dfa1664a97d799fe672bf5364282f5ff0d0bec
KOMGRICH SRISAK <6131305035@lamduan.mfu.ac.th>
2021-09-09 21:43:35 +0700

    CRUD complete

--- functions/index.js
+++ functions/index.js
@@ -31,25 +31,10 @@ let Student_obj = {
 }
 //exports const webApi = functions.https.onRequest(main);
 
-exports.addStudent = functions.https.onRequest( async (req,res) => {
+// Get All Students service
+    exports.getAllStudent = functions.https.onRequest( async ( req , res ) => {
         try {
-            const student = {
-                name : req.body['name'],
-                phonenumber : req.body['phonenumber'],
-                age : req.body['age'],
-                retire : req.body['retire']
-            }
-
-            const new_doc = await database.collection("school").add(student);
-            res.status(201).send(`Create a new user : ${new_doc}`)
-        } catch (error) {
-            res.status(400).send('User should contain all things!')
-        }
-    });
-
-exports.getAllStudent = functions.https.onRequest( async (req,res) => {
-        try {
-            const student_query_snapshot = await database.collection("school").get();
+            const student_query_snapshot = await database.collection(userCollection).get();
             const school = [];
             student_query_snapshot.forEach(
                 (doc) => {
@@ -65,3 +50,51 @@ exports.getAllStudent = functions.https.onRequest( async (req,res) => {
         }
     } )
 
+// Get Specific Student services
+    exports.getSpecificStudent = functions.https.onRequest( async ( req , res ) => {
+        try {
+            const Id = req.params.id
+            database.collection(userCollection).doc(Id).get()
+            .then( student => { 
+                if(!student.exists) throw new Error('Student not found');
+                res.status(200).json({ id : student.id , data : student.data() })
+            })
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    })      
+
+// Add New Student service
+    exports.addStudent = functions.https.onRequest( async ( req , res ) => {
+        try {
+            const student = {
+                name : req.body['name'],
+                phonenumber : req.body['phonenumber'],
+                age : req.body['age'],
+                retire : req.body['retire']
+            }
+
+            const new_doc = await database.collection(userCollection).add(student);
+            res.status(201).send(`Create a new user : ${new_doc}`)
+        } catch (error) {
+            res.status(400).send('User should contain all things!')
+        }
+    });
+
+// Update Specific Student service
+    exports.updateSpecificStudent = functions.https.onRequest( async ( req , res ) => {
+        const Id = req.params.id;
+        await database.collection(userCollection).doc(Id).set(req.body,{ merge :true })
+        .then( () => res.json({ id : Id }) )
+        .catch( (error) => res.status(500).send(error) )
+    });
+
+// Delete Specific Student service
+    exports.deleteSpecificStudent = functions.https.onRequest( async ( req , res ) => {
+        const Id = req.params.id
+        database.collection(userCollection).doc(Id).delete()
+        .then( () => res.status(204).send("Document successfully deleted!"))
+        .catch( function (error) {
+            res.status(500).send(error);
+        })
+    });
